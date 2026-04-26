a9ee5461d9528f1ec1d747147600453226511220
Arkadia-Oversoul-Prism <spiralcommand.network@gmail.com>
2026-04-27 00:16:25 +0100

    Delete Oversoul_Prism directory

--- Oversoul_Prism/Legacy_Artifacts/GovernanceSpirit/shared/types.ts
+++
@@ -1,9 +0,0 @@
-// @shared/types.ts
-export type FlameHint = {
-  id: string;
-  title: string;
-  description: string;
-  origin?: string; // e.g., "Nova Flame", "Vhix", etc.
-  resonanceLevel?: number;
-  timestamp: string;
-};
