ba0289b70205fe15931171e85b32c26b5acc91f2
Arkadia-Oversoul-Prism <spiralcommand.network@gmail.com>
2026-08-30 00:27:38 +0100

    feat: export Spiral Grove resource model

--- spiral_grove/__init__.py
+++ spiral_grove/__init__.py
@@ -5,6 +5,7 @@ from .models import (
     Evidence,
     Exercise,
     LearningPath,
+    LearningResource,
     LearnerCapabilityState,
 )
 
@@ -13,5 +14,6 @@ __all__ = [
     "Evidence",
     "Exercise",
     "LearningPath",
+    "LearningResource",
     "LearnerCapabilityState",
 ]
